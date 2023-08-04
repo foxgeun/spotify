3a6765be8edc01cfbd92030a43b36130e3d95b31
foxgeun <p31644@gmail.com>
2023-08-04 17:56:06 +0900

    update

---
+++ src/main/java/com/example/spotify/controller/SpotifyController.java
@@ -0,0 +1,25 @@
+package com.example.spotify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spotify.search.MusicSearch;
+import com.example.spotify.token.CreateToken;
+
+@RestController
+public class SpotifyController {
+
+    // CreateToken 객체를 생성하여 access token을 얻어옵니다.
+    private final CreateToken createToken = new CreateToken();
+    private final String accessToken = createToken.accesstoken();
+
+    @GetMapping("/search")
+    public ResponseEntity<String> searchSong(@RequestParam("q") String query) {
+        MusicSearch musicSearch = new MusicSearch();
+        String response = musicSearch.search(accessToken, query);
+        System.out.println(accessToken +  query);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/example/spotify/token/CreateToken.java
@@ -0,0 +1,32 @@
+package com.example.spotify.token;
+
+import java.io.IOException;
+
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import org.apache.hc.core5.http.ParseException;
+
+public class CreateToken {
+
+    private static final String CLIENT_ID = "e78acd84cdc4413d903974a5077184e6";
+    private static final String CLIENT_SECRET = "b5bc0f9f1a5141f980f08f49ccd5831a";
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(CLIENT_ID).setClientSecret(CLIENT_SECRET).build();
+
+    public static String accesstoken() {
+        ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+        try {
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+            return spotifyApi.getAccessToken();
+
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+            return "error";
+        }
+    }
+
+}
